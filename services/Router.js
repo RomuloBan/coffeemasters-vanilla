ef6301cf80f138bfa05a09d3e933d4975cf1fe00
romuloban <romulo88@hotmail.com>
2023-07-21 15:29:17 +0200

    add basic router logic

--- services/Router.js
+++ services/Router.js
@@ -14,6 +14,25 @@ const Router = {
         if (addToHistory) {
             history.pushState({ route }, '', route);
         }
+        let pageElement = null;
+        switch(route) {
+            case '/':
+                pageElement = document.createElement('h1');
+                pageElement.textContent = 'Menu';
+                break;
+            case '/order':
+                pageElement = document.createElement('h1');
+                pageElement.textContent = 'Your Order';
+                break;
+        }
+        if (pageElement) {
+            const main = document.querySelector('main');
+            main.innerHTML = '';
+            main.appendChild(pageElement);
+            window.scrollX = 0;
+            window.scrollY = 0;
+        }
+
     }
 };
 
