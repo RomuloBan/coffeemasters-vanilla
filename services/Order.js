3d06879ff38fde680bdf18b107001f676dfe5322
romuloban <romulo88@hotmail.com>
2023-07-28 16:19:59 +0200

    add order page

--- services/Order.js
+++ services/Order.js
@@ -3,15 +3,15 @@ import { getProductById } from "./Menu.js";
 
 export async function addToCart(id) {
     const product = await getProductById(id);
-    const results = app.store.cart.filter(order => order.id === id);
+    const results = app.store.cart.filter(order => order.product.id === id);
     if (results.length === 1) {
         // Update de quantity
-        app.store.cart = app.store.cart.map(p => p.id === id ? {...p, quantity: p.quantiy+1} : p);
+        app.store.cart = app.store.cart.map(order => order.product.id === id ? {...order, quantity: order.quantity+1} : order);
     } else {
         app.store.cart = [...app.store.cart, { product, quantity: 1 }];
     }
 }
 
 export function removeFromCart(id) {
-    app.store.cart = app.store.cart.filter(p => p.id !== id);
+    app.store.cart = app.store.cart.filter(order => order.product.id !== id);
 }
